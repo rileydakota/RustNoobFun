0d3636f1d99ed83a5b3fcbb3df90bb6bfb643e31
rileydakota <73837129+rileydakota@users.noreply.github.com>
2022-04-07 20:50:55 +0000

    initial commit of number guessing game

--- number_guessing_game/src/main.rs
+++ number_guessing_game/src/main.rs
@@ -1,3 +1,32 @@
+use rand::Rng;
+use std::io;
+
+const MAX_RAND: i8 = 20;
+//const MAX_GUESSES: i8 = 5;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+
+    println!("Guess a number between 1 and {}!", MAX_RAND);
+    let answer = rng.gen_range(0..MAX_RAND);
+    loop {
+        let mut player_answer = String::new();
+
+        io::stdin()
+            .read_line(&mut player_answer)
+            .expect("failed to collect answer")
+            .to_string();
+
+        let player_number_answer: i8 = player_answer.trim().parse().unwrap();
+
+        match player_number_answer {
+            a if a == answer => {
+                println!("Correct! You win!");
+                break;
+            }
+            a if a > answer => println!("Wrong! {} is too high!", a),
+            a if a < answer => println!("Wrong! {} is too low!", a),
+            _ => panic!("unknown error, exiting"),
+        }
+    }
 }
